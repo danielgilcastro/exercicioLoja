840d9eda89fbd522f7412a556b4ee9a515864252
Daniel <danielgilcastro@gmail.com>
2025-07-07 14:09:29 -0300

    inicio vitrine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { VitrineComponent } from './components/vitrine/vitrine.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: VitrineComponent
+    },
+    {
+        path: 'produtos',
+        component: ProdutosComponent
+    },
+    {
+        path: 'contato',
+        component: ContatoComponent
+    }
+
+];
